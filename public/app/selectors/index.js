b917be2c744434e1f3e38df8bf047a89077a5dbf
vincent-tr <vincent@stoic.com>
2018-08-27 08:54:55 +0200

    sort albums by name

--- public/app/selectors/index.js
+++ public/app/selectors/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export const getAlbums = state => state.albums;
+export const getAlbums = state => state.albums && sort(state.albums, a => a.name);
 
 export const isAlbum = state => !!state.album;
 export const getAlbum = state => state.album;
@@ -18,4 +18,27 @@ function prevItem(array, item) {
 function nextItem(array, item) {
   const index = array.indexOf(item);
   return array[index + 1];
+}
+
+function sort(array, accessor = (a => a), comparator = defaultComparator) {
+  const res = [ ... array ];
+  res.sort((a, b) => comparator(accessor(a), accessor(b)));
+  return res;
+}
+
+// https://github.com/facebook/immutable-js/blob/e65e5af806ea23a32ccf8f56c6fabf39605bac80/src/Operations.js
+function defaultComparator(a, b) {
+  if (a === undefined && b === undefined) {
+    return 0;
+  }
+
+  if (a === undefined) {
+    return 1;
+  }
+
+  if (b === undefined) {
+    return -1;
+  }
+
+  return a > b ? 1 : a < b ? -1 : 0;
 }
