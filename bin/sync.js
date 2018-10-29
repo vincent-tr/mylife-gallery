914753ba390abefea638443c385da6053b180d99
vincent-tr <vincent@stoic.com>
2018-10-29 10:51:09 +0100

    wip sync

--- bin/sync.js
+++ bin/sync.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { run } = require('../lib/sync');
+const { sync } = require('../lib/sync');
 const { close } = require('../lib/repository');
 
 async function start() {
   try {
-    await run();
+    await sync();
   } catch(err) {
     console.error(err); // eslint-disable-line no-console
   } finally {
