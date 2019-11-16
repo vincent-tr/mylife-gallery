1f5c15964121a3a79c0c18ef5bb1b099bd475feb
vincent-tr <vincent@stoic.com>
2019-11-16 10:14:47 +0100

    adapt stuff

--- bin/server.js
+++ bin/server.js
@@ -3,41 +3,9 @@
 'use strict';
 
 require('../lib/init');
-const { createLogger } = require('mylife-tools-server');
+const { runServices } = require('mylife-tools-server');
+const { webApiFactory } = require('../lib/web');
 
-const logger = createLogger('mylife:gallery:bin:server');
-const Server = require('../lib/server');
-
-const dev = process.argv.includes('--dev');
-const options = { dev };
-
-let server;
-
-async function start() {
-
-  logger.info(`Starting server (options=${JSON.stringify(options)})`);
-
-  try {
-    server = new Server();
-    await server.init(options);
-  } catch(err) {
-    console.error('Error starting server', err); // eslint-disable-line no-console
-    process.exit();
-  }
-}
-
-async function stop() {
-  try {
-    await server.close();
-    server = null;
-  } catch(err) {
-    console.error('Error closing server', err); // eslint-disable-line no-console
-  }
-
-  process.exit();
-}
-
-process.on('SIGINT', stop);
-process.on('SIGTERM', stop);
-
-start();
+const services = ['web-server', 'database'];
+const parameters = { webApiFactory };
+runServices({ services, ... parameters });
