36a4b69713b27fda7fbf995e704b2705c23601d9
vincent-tr <vincent@stoic.com>
2019-11-16 10:30:40 +0100

    sync server

--- bin/server.js
+++ bin/server.js
@@ -5,7 +5,8 @@
 require('../lib/init');
 const { runServices } = require('mylife-tools-server');
 const { webApiFactory } = require('../lib/web');
+require('../lib/sync');
 
-const services = ['web-server', 'database'];
+const services = ['web-server', 'sync-server', 'database'];
 const parameters = { webApiFactory };
 runServices({ services, ... parameters });
