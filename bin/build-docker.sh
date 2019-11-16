beec979c95d8a71020bd38661743fa2bcaba59f9
vincent-tr <vincent@stoic.com>
2019-11-16 10:15:00 +0100

    cleanup

--- bin/build-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env /bin/bash
-
-package_version=$(node -p "require(path.join(process.cwd(), './package.json')).version")
-docker build -t "vincenttr/mylife-gallery:$package_version" --build-arg "package_version=$package_version" - < "tools/Dockerfile"
