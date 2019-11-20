8aa90d75f08024eb621065d0501deae15941e290
vincent-tr <vincent@stoic.com>
2019-11-20 09:19:32 +0100

    metadata

--- shared/metadata/datatypes.js
+++ shared/metadata/datatypes.js
@@ -13,5 +13,20 @@ module.exports = [
       { id: 'type', name: 'Type du document', datatype: 'name', constraints: ['not-null'] },
       { id: 'id', name: 'Identifiant du document', datatype: 'identifier', constraints: ['not-null'] }, // as identifier because it is a polymorph reference
     ]
+  },
+  {
+    id: 'image-tag', structure: [
+      { id: 'person', name: 'Personne', datatype: 'person' },
+      { id: 'left', name: 'Placement gauche', datatype: 'count' },
+      { id: 'top', name: 'Placement haut', datatype: 'count' },
+      { id: 'width', name: 'Largeur', datatype: 'count' },
+      { id: 'height', name: 'Hauteur', datatype: 'count' },
+    ]
+  },
+  {
+    id: 'gps', structure: [
+      { id: 'latitude', name: 'Latitude', datatype: 'real' },
+      { id: 'longitude', name: 'Longitude', datatype: 'real' },
+    ]
   }
 ];
--- shared/metadata/entities/image.js
+++ shared/metadata/entities/image.js
@@ -8,9 +8,9 @@ module.exports = {
     { id: 'perceptualHash', name: 'Hash perception', datatype: 'name', constraints: ['not-null', 'not-empty'] },
     { id: 'thumbnail', name: 'Miniature', datatype: 'identifier' }, // we do not directly reference thumbnail because it is not loaded as store collection
     { id: 'metaDate', name: 'Date métadonnées', datatype: 'datetime' },
-    { id: 'gps', name: 'Coordonnées GPS', datatype: 'any' },
+    { id: 'gps', name: 'Coordonnées GPS', datatype: 'gps' },
     { id: 'width', name: 'Largeur', datatype: 'count' },
     { id: 'height', name: 'Hauteur', datatype: 'count' },
-    { id: 'persons', name: 'Personnes', datatype: 'any' }, // array of { personId, rectangle }
+    { id: 'persons', name: 'Personnes', datatype: 'list:image-tag' },
   ]
 };
