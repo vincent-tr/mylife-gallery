c703a02f94b866434166417579f6327aef23c7f8
vincent-tr <vincent@stoic.com>
2019-11-20 17:42:48 +0100

    plug stuff

--- bin/server.js
+++ bin/server.js
@@ -5,8 +5,10 @@
 require('../lib/init');
 const { runServices } = require('mylife-tools-server');
 const { webApiFactory } = require('../lib/web');
+const metadataDefintions = require('../shared/metadata');
+const storeConfiguration = require('../lib/store-configuration');
 require('../lib/sync');
 
-const services = ['web-server', 'sync-server', 'database'];
-const parameters = { webApiFactory };
+const services = ['web-server', 'sync-server', 'store', 'database'];
+const parameters = { webApiFactory, metadataDefintions, storeConfiguration };
 runServices({ services, ... parameters });
