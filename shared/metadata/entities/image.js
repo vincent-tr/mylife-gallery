62b41bf74de77637b4440bdcbee90c42c5d557d1
vincent-tr <vincent@stoic.com>
2019-11-25 09:38:43 +0100

    image loader

--- shared/metadata/entities/image.js
+++ shared/metadata/entities/image.js
@@ -7,6 +7,7 @@ module.exports = {
   fields: [
     { id: 'perceptualHash', name: 'Hash perception', datatype: 'name', constraints: ['not-null', 'not-empty'] },
     { id: 'thumbnail', name: 'Miniature', datatype: 'identifier' }, // we do not directly reference thumbnail because it is not loaded as store collection
+    { id: 'mime', name: 'Type mime', datatype: 'name' },
     { id: 'metadata', name: 'Métadonnées', datatype: 'image-metadata' },
     { id: 'width', name: 'Largeur', datatype: 'count' },
     { id: 'height', name: 'Hauteur', datatype: 'count' },
