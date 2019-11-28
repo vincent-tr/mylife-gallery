523445d94a1416ef138fc18752bef87c43d9d5c3
vincent-tr <vincent@stoic.com>
2019-11-28 15:15:17 +0100

    stat entity

---
+++ shared/metadata/entities/stat.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  id: 'stat',
+  parent: 'base',
+  name: 'Stat',
+  fields: [
+    { id: 'value', name: 'Valeur', datatype: 'any' },
+    { id: 'code', name: 'Code', datatype: 'name' },
+  ]
+};
