29657d167be6afc0fd5261a1deaeff11ce394aca
vincent-tr <vincent@stoic.com>
2019-11-28 09:12:08 +0100

    move to default when fail to load as image/video

--- shared/metadata/entities/other.js
+++ shared/metadata/entities/other.js
@@ -4,5 +4,7 @@ module.exports = {
   id: 'other',
   parent: 'document',
   name: 'Autre',
-  fields: []
+  fields: [
+    { id: 'loadingError', name: 'Erreur au chargement', descriptions: 'Pour les images/video, log de l\'erreur au chargement qui a conduit leur intégration comme \'Autre\'' , datatype: 'text' },
+  ]
 };
