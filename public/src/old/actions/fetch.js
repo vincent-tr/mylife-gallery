cfe7f7513a086128c424f18547e043df58b121e5
vincent-tr <vincent@stoic.com>
2019-12-03 16:11:12 +0100

    remove old stuff

--- public/src/old/actions/fetch.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import { createAction } from 'redux-actions';
-import actionTypes from '../constants/action-types';
-
-export const fetchAlbums = createAction(actionTypes.SET_ALBUMS);
-export const fetchAlbum  = createAction(actionTypes.SET_ALBUM);
